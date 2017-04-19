665bf27d56e520997ff749a609bda69c1b6056e0
krzysztof-osiecki <krzysztof@live.fr>
2017-04-19 20:51:02 +0200

    dokumentacja + opis

--- stream.hpp
+++ stream.hpp
@@ -21,28 +21,99 @@ namespace stream {
     class stream {
 
     public:
-        stream();
 
+        /**
+         * Konstruktor tworzacy strumien z instancji std::vector
+         *
+         * @param data wektor ktory zostanie opakowany strumieniem
+         */
         stream(const std::vector<T> &data);
 
-        bool allMatch();
-
-        bool anyMatches();
-
-        T find();
-
+        /**
+         * Operacja nakladajaca na strumien filtr okreslony zadana funkcja.
+         * Operacja nieterminalna
+         *
+         * @param predicate predykat okreslajacy warunek konieczny do znalezienia sie w wynikowym strumieniu
+         * @return strumien z zaaplikowana funkcja filtrujaca
+         */
         stream<T> *filter(std::function<bool(T)> predicate);
 
-        void foreach(std::function<void(T)> mappingFunction);
-
-        T reduce(std::function<T(T, T)> reductorFunction);
-
-        std::vector<T> *toVector();
-
+        /**
+         * Operacja konwerujaca strumien, do typu wskazanego przez funkcje mapujaca, poprzez
+         * zaaplikowanie jej do kazdego elementu strumienia.
+         * Operacja terminalna
+         *
+         * @tparam R typ nowego strumienia
+         * @param mappingFunction funkcja mapujaca
+         * @return nowy strumien z elementami bedacymi wynikiem funkcji mapujacej
+         */
         template<class R>
         stream<R> *map(std::function<R(T)> mappingFunction);
 
+        /**
+         * Operacja redukcji strumienia. Wskazana funkcja bedzie wywolywana na
+         * elementach strumienia, w postaci fun(poprzedni_wynik, aktualny_element),
+         * az do wyczerpania elementow.
+         * Operacja terminalna
+         *
+         * @param reductorFunction funkcja odpowiadajaca za redukcje elementow
+         * @return zredukowana wartosc ostatniego wykonania funkcji
+         */
+        T reduce(std::function<T(T, T)> reductorFunction);
+
+        /**
+         * Operacja aplikuje zadana funkcje do kazdego elementu strumienia
+         * Operacja terminalna
+         *
+         * @param exectutionFunction funkcja do wykonania na kazdym elemencie
+         */
+        void foreach(std::function<void(T)> exectutionFunction);
+
+        /**
+         * Operacja okreslajaca czy wszystkie elementy danego strumienia spelniaja
+         * zaaplikowane do niego predykaty operacji filter.
+         * Operacja terminalna
+         *
+         * @return true jesli operacje filter nie wykluczyly zadnego z elementow strumienia
+         *         false w pozostalych przypadkach
+         */
+        bool allMatch();
+
+        /**
+         * Operacja okreslajaca czy choc jeden element danego strumienia spelnia
+         * zaaplikowane do niego predykaty operacji filter.
+         * Operacja terminalna
+         *
+         * @return true jesli operacje filter pozostawily choc jeden element strumienia
+         *         false w pozostalych przypadkach
+         */
+        bool anyMatches();
+
+        /**
+         * Operacja znajdujaca pierwszy element strumienia.
+         * Operacja terminalna
+         *
+         * @return znaleziony element, NULL dla pustego strumienia
+         */
+        T find();
+
+        /**
+         * Operacja uzytkowa, przechodzi strumien wypisujac jego zawartosc
+         * na standardowe wyjscie.
+         * Operacja nieterminalna
+         *
+         * @return ten sam strumien wejsciowy
+         */
         stream<T> *peek();
+
+        /**
+         * Operacja powrotu ze strumienia do std::vector. Aplikowane sa wszsytkie
+         * operacje filter i zwracany nowy obiekt vectora.
+         * Operacja terminalna
+         *
+         * @return std::vector zawierajacy elementy strumienia
+         */
+        std::vector<T> *toVector();
 
     protected:
         stream(const std::vector<T> &data, std::vector<streamOperation<bool(T)> *> *predicates);
@@ -78,13 +149,6 @@ namespace stream {
     template<class T>
     stream<T>::stream(const std::vector<T> &data) {
         this->underlyingVector = new std::vector<T>(data);
-        this->predicates = new std::vector<streamOperation<bool(T)> *>();
-        this->consumed = false;
-    }
-
-    template<class T>
-    stream<T>::stream() {
-        this->underlyingVector = new std::vector<T>();
         this->predicates = new std::vector<streamOperation<bool(T)> *>();
         this->consumed = false;
     }
@@ -144,7 +208,7 @@ namespace stream {
     }
 
     template<class T>
-    void stream<T>::foreach(std::function<void(T)> foreachOperation) {
+    void stream<T>::foreach(std::function<void(T)> exectutionFunction) {
         std::vector<T> *filtered = toVector();
         for (typename std::vector<T>::iterator it = filtered->begin();
              it != filtered->end();) {
